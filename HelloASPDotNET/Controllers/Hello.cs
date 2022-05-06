8cdb74ccfd26e8d5daf9f13334cc470b9e0da96d
Zach Robles <45700544+zdrobles@users.noreply.github.com>
2022-05-06 11:21:07 -0400

    Completed code-along

---
+++ HelloASPDotNET/Controllers/Hello.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloASPDotNET.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/hello")]
+        public IActionResult Welcome(string name = "World")
+        {
+            ViewBag.person = name;
+            return View();
+        }
+    }
+}
